19ddb6bfb8d6f5f32999de80f57421d87767f0dd
Tomáš Fulajtár <tomas.fulajtar@rws.com>
2021-05-28 14:51:14 +0200

    Strategy Pattern - initial implementation.

---
+++ StrategyPattern/IDressStrategy.cs
@@ -0,0 +1,35 @@
+﻿namespace DesignPatterns
+{
+    public interface IDressStrategy
+    {
+
+        public string GetCoat();
+        public string GetHeadcover();
+        public string GetTroussers();
+        public string GetShoes();
+    }
+
+    public class RainyDressStrategy : IDressStrategy
+    {
+        public string GetCoat() => "Rain coat";
+        
+        public string GetHeadcover() => "Rain hat";        
+
+        public string GetShoes()    => "Rain boots";        
+
+        public string GetTroussers() =>  "Swimsuit";
+    }
+
+    public class SunnyDressStrategy : IDressStrategy
+    {
+        public string GetCoat() => "Naked";
+
+        public string GetHeadcover() => "Kshilt";
+
+        public string GetShoes() => "Bare foot";
+
+        public string GetTroussers() => "Shorts";
+    }
+
+}
+
---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dressPicker = new DressPicker();
+
+            dressPicker.SetDressStrategy(new SunnyDressStrategy());
+
+            Console.WriteLine($"Suggested coat is {dressPicker.GetCoat()}");
+            Console.WriteLine($"Suggested hat style is {dressPicker.GetHeadcover()}");
+            Console.WriteLine($"Suggested shoe is {dressPicker.GetShoes()}");
+            Console.WriteLine($"Suggested troussers is {dressPicker.GetTroussers()}");            
+        }
+    }
+
+    public enum Weather
+    {
+        Rainy,
+        Sunny,
+        Snowy,
+        Windy
+    }    
+
+    public class DressPicker
+    {    
+        private IDressStrategy _dressStrategy;
+
+        public void SetDressStrategy(IDressStrategy strategy)
+        {
+            _dressStrategy = strategy;
+        }
+
+        public string GetShoes()
+        {
+            return _dressStrategy.GetShoes();
+        }
+
+        public string GetTroussers ()
+        {
+            return _dressStrategy.GetTroussers();
+        }
+
+        public string GetCoat ()
+        {
+            return _dressStrategy.GetCoat();
+        }
+
+        public string GetHeadcover ()
+        {
+            return "Coat head cover unsupported";
+        }
+
+      }
+}
