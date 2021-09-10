7a1ac0a0cba15236e22813d01dd4ff085a1fe34c
Josef Hošek <Josef.Hosek@rws.com>
2021-09-10 08:29:01 +0200

    Add adapter pattern

---
+++ AdapterPattern/FileAdapterExample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public class FileAdapterExample
+    {
+        private readonly IFileAdapter _fileAdapter;
+
+        public FileAdapterExample(IFileAdapter fileAdapter)
+        {
+            _fileAdapter = fileAdapter;
+        }
+
+        public string Read(string path)
+        {
+
+            if (_fileAdapter.Exists(path))
+            {
+                return _fileAdapter.ReadAllText(path);
+            }
+
+            return string.Empty;
+        }
+    }
+
+    public interface IFileAdapter
+    {
+        bool Exists(string path);
+
+        string ReadAllText(string path);
+    }
+
+    public class FileAdapter : IFileAdapter
+    {
+        public bool Exists(string path) => File.Exists(path);
+
+        public string ReadAllText(string path) => File.ReadAllText(path);
+    }
+}
---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mySource = Sources.File;
+            IReaderAdapter readerAdapter = mySource switch
+            {
+                Sources.File => new FileReaderAdapter(),
+                Sources.Database => new DatabaseReaderAdapter(),
+                Sources.Web => new WebReaderAdapter(),
+                _ => throw new NotSupportedException("ne"),
+            };
+            var result = readerAdapter.Read();
+
+            //switch (mySource)
+            //{
+            //    case Sources.File:
+            //        var fileReader = new FileReader();
+            //        var file = fileReader.ReadFile();
+            //        break;
+            //    case Sources.Database:
+            //        var dbReader = new DatabaseReader();
+            //        var db = dbReader.ReadDatabase();
+            //        break;
+            //    case Sources.Web:
+            //        var webReader = new WebReader();
+            //        var web = webReader.ReadWeb();
+            //        break;
+            //    default:
+            //        throw new NotImplementedException("nemam");
+            //}
+        }
+    }
+
+    public interface IReaderAdapter
+    {
+        string Read();
+    }
+
+    public class FileReaderAdapter : IReaderAdapter
+    {
+        public string Read()
+        {
+            var fileReader = new FileReader();
+            return fileReader.ReadFile();
+        }
+    }
+    public class DatabaseReaderAdapter : IReaderAdapter
+    {
+        public string Read()
+        {
+            var dbReader = new DatabaseReader();
+            return dbReader.ReadDatabase();
+        }
+    }
+    public class WebReaderAdapter : IReaderAdapter
+    {
+        public string Read()
+        {
+            var webReader = new WebReader();
+            return webReader.ReadWeb();
+        }
+    }
+
+    public enum Sources
+    {
+        File,
+        Database,
+        Web
+    }
+
+    #region third party - not ours!
+    public class FileReader
+    {
+        public string ReadFile()
+        {
+            return string.Empty;
+        }
+    }
+
+    public class DatabaseReader
+    {
+        public string ReadDatabase()
+        {
+            return string.Empty;
+        }
+    }
+
+    public class WebReader
+    {
+        public string ReadWeb()
+        {
+            return string.Empty;
+        }
+    }
+    #endregion
+
+}
