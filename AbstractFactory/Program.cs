a2daac0ecefaee2876a8cb324944725d2a98866e
Petr Pucek <ppucek@moravia.com>
2021-09-15 13:01:19 +0200

    Abstract factory WIP

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card card = new CardFactory().CreateCard(CardType.Debit);
+        }
+    }
+
+    internal class CardFactory
+    {
+        public Card CreateCard(CardType cardType)
+        {
+            switch (cardType)
+            {
+                case CardType.Debit:
+                    return new DebitCard();
+                case CardType.Credit:
+                    return new CreditCard(1000);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(cardType), cardType, null);
+            }
+        }
+    }
+
+    internal class CreditCard : Card
+    {
+        private int Limit { get; set; }
+        public override void TakeMoney(int amount)
+        {
+            if (amount <= Limit)
+            {
+                Limit -= amount;
+            }
+            else
+            {
+                throw new Exception("Limit overreached.");
+            }
+        }
+
+        public CreditCard(int limit)
+        {
+            Limit = limit;
+        }
+    }
+
+    internal class DebitCard : Card
+    {
+        public override void TakeMoney(int amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal abstract class Card
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Id { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public int Ccv { get; set; }
+
+        public abstract void TakeMoney(int amount);
+    }
+
+    internal enum CardType
+    {
+        Debit,
+        Credit
+    }
+}
---
+++ AbstractFactory/Program2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Program2
+    {
+        void Main(string[] args)
+        {
+            var bankServicesFactory = new FreeBankServicesFactory();
+            var card = bankServicesFactory.CreateCard();
+            var account = bankServicesFactory.CreateAccount();
+        }
+    }
+
+    interface IBankServicesFactory
+    {
+        Card CreateCard();
+        Account CreateAccount();
+    }
+
+    class PremiumBankServicesFactory : IBankServicesFactory
+    {
+        public Card CreateCard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account CreateAccount()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class FreeBankServicesFactory : IBankServicesFactory
+    {
+        public Card CreateCard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account CreateAccount()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    abstract class Account
+    {
+    }
+}
