c5a9c3258ab713a7909e12d075d3bb512502d2a2
Petr Jankovsky <petr.jankovsky@rws.com>
2021-09-29 12:56:41 +0200

    Builder initialization

---
+++ BuilderPattern/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderPattern
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Author{ get; set; }
+        public string NumberOfPages { get; set; }
+    }
+}
---
+++ BuilderPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuilderPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new List<Book>
+            {
+                new Book{Author="Arthur C. Doyle",Name="Baskervil Dog", NumberOfPages="100"},
+                new Book{Author="Robert C. Martin",Name="Clean Code", NumberOfPages="324"},
+                new Book{Author="Frenk Abagnale",Name="Catch me if you can", NumberOfPages="650"},
+                new Book{Author="Karel Capek",Name="R.U.R.", NumberOfPages="50"},
+                new Book{Author="Jorge Orwell",Name="1984", NumberOfPages="350"}
+            };
+        }
+    }
+}
