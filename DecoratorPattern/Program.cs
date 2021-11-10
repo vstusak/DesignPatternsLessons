c235bce70eb2c92cb6739dc7d37846a0059c0231
Milan Karasek <milank@moravia.com>
2021-11-10 11:59:45 +0100

    Decorator WIP

---
+++ DecoratorPattern/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Threading;
+
+namespace DecoratorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IWatcher watcher = new Watcher();
+            watcher = new TinderNotificationForWatcher(watcher);
+            watcher.EventRaised();
+
+            Console.ReadLine();
+        }
+    }
+
+
+    interface IWatcher
+    {
+        void EventRaised();
+    }
+
+    class Watcher : IWatcher
+    {
+        public void EventRaised()
+        {
+            Console.WriteLine("Event has been raised.");
+        }
+    }
+
+    interface IWatcherDecorator : IWatcher
+    {
+        //empty
+    }
+
+    class TinderNotificationForWatcher : IWatcherDecorator
+    {
+        private readonly IWatcher _decoratedWatcher;
+
+        public TinderNotificationForWatcher(IWatcher decoratedWatcher)
+        {
+            _decoratedWatcher = decoratedWatcher;
+        }
+
+        public void EventRaised()
+        {
+            Console.WriteLine("-- Tinder Notification --");
+            _decoratedWatcher.EventRaised();
+            Console.WriteLine("-- /Tinder Notification --");
+            Thread.Sleep(1000);
+        }
+    }
+
+    class FacebookNotificationForWatcher : IWatcherDecorator
+    {
+        private readonly IWatcher _decoratedWatcher;
+
+        public FacebookNotificationForWatcher(IWatcher decoratedWatcher)
+        {
+            _decoratedWatcher = decoratedWatcher;
+        }
+
+        public void EventRaised()
+        {
+            Console.WriteLine("-- Facebook Notification --");
+            _decoratedWatcher.EventRaised();
+            Console.WriteLine("-- /Facebook Notification --");
+            Thread.Sleep(1000);
+        }
+    }
+
+    class EmailNotificationForWatcher : IWatcherDecorator
+    {
+        private readonly IWatcher _decoratedWatcher;
+
+        public EmailNotificationForWatcher(IWatcher decoratedWatcher)
+        {
+            _decoratedWatcher = decoratedWatcher;
+        }
+
+        public void EventRaised()
+        {
+            Console.WriteLine("-- Email Notification --");
+            _decoratedWatcher.EventRaised();
+            Console.WriteLine("-- /Email Notification --");
+            Thread.Sleep(1000);
+        }
+    }
+
+    class PerformanceMeasurementForWatcher : IWatcherDecorator
+    {
+        private readonly IWatcher _decoratedWatcher;
+
+        public PerformanceMeasurementForWatcher(IWatcher decoratedWatcher)
+        {
+            _decoratedWatcher = decoratedWatcher;
+        }
+
+        public void EventRaised()
+        {
+            Console.WriteLine($"-- Perf: {DateTime.Now} --");
+            _decoratedWatcher.EventRaised();
+            Console.WriteLine($"-- End Perf: {DateTime.Now} --");
+        }
+    }
+
+
+
+}
