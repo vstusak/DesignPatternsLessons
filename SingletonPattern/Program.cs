a82de90b591efcf3b6a25a045971afef9b1e5337
Petr Jankovsky <petr.jankovsky@rws.com>
2021-12-15 12:10:07 +0100

    Singleton pattern ended.

--- SingletonPattern/Program.cs
+++ SingletonPattern/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SingletonPattern
@@ -16,22 +17,71 @@ namespace SingletonPattern
             var fileNames = new List<string>() { "one", "two", "three" };
             var options = new ParallelOptions() { MaxDegreeOfParallelism = 3 };
 
+            Console.WriteLine("BeforeParalleForEach");
+
             Parallel.ForEach(fileNames, options, fileName =>
             {
-                var helper = FileHelper.GetInstance();
+                var helper = FileHelper3.GetInstance();
                 var result = helper.ReadFile(fileName);
 
                 Console.WriteLine(result);
             });
-
-
         }
     }
 
+    public sealed class FileHelper3
+    {
+        private static readonly Lazy<FileHelper3> _instance = new Lazy<FileHelper3>(() => new FileHelper3());
+
+        private FileHelper3()
+        {
+            Thread.Sleep(10000);
+            Console.WriteLine("calling constructor ...");
+        }
+
+        public static FileHelper3 GetInstance()
+        {
+            Console.WriteLine("GetInstance");
+            return _instance.Value;
+        }
+
+        public string ReadFile(string fileName)
+        {
+            Console.WriteLine("Hello World!");
+            return fileName;
+        }
+    }
+
+    public sealed class FileHelper2
+    {
+        private static readonly FileHelper2 _instance = new FileHelper2();
+
+        static FileHelper2()
+        {
+            Console.WriteLine("calling static constructor ...");
+        }
+        private FileHelper2()
+        {
+            Console.WriteLine("calling constructor ...");
+        }
+
+        public static FileHelper2 GetInstance()
+        {
+            Console.WriteLine("GetInstance");
+            return _instance;
+        }
+
+        public string ReadFile(string fileName)
+        {
+            Console.WriteLine("Hello World!");
+            return fileName;
+        }
+    }
 
     public sealed class FileHelper
     {
         private static FileHelper _instance;
+        private static readonly Object peadlock = new object();
 
         private FileHelper()
         {
@@ -40,7 +90,18 @@ namespace SingletonPattern
 
         public static FileHelper GetInstance()
         {
-            return _instance ??= new FileHelper();
+            //return _instance ?? _instance = new FileHelper();
+            if (_instance == null)
+            {
+                lock (peadlock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new FileHelper();
+                    }
+                }
+            }
+            return _instance;
         }
 
         public string ReadFile(string fileName)
