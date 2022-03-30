fd7f06602bd5cfe3c031971c2421d0c519398492
Petr Pucek <ppucek@moravia.com>
2022-03-30 11:58:11 +0200

    RepositoryDesignPattern Tests

--- RepositoryDesignPattern/Commands/ICommand.cs
+++ RepositoryDesignPattern/Commands/ICommand.cs
@@ -15,7 +15,6 @@ namespace RepositoryDesignPattern.Commands
     {
         private readonly IRepository<Product> _repository;
         private readonly Guid _productId;
-        private Product _product;
 
         public BuyCommand(IRepository<Product> repository, Guid productId)
         {
@@ -25,23 +24,25 @@ namespace RepositoryDesignPattern.Commands
 
         public bool CanExecute()
         {
-            _product = _repository.Get(_productId);
-            return _product.Quantity > 0;
+            var product = _repository.Get(_productId);
+            return product.Quantity > 0;
         }
 
         public void Execute()
         {
             Console.WriteLine("Executing the execution.");
-            _product.Quantity -= 1;
-            _repository.Update(_product);
+            var product = _repository.Get(_productId);
+            product.Quantity -= 1;
+            _repository.Update(product);
             _repository.SaveChanges();
         }
 
         public void Undo()
         {
             Console.WriteLine("calling Undo");
-            _product.Quantity += 1;
-            _repository.Update(_product);
+            var product = _repository.Get(_productId);
+            product.Quantity += 1;
+            _repository.Update(product);
             _repository.SaveChanges();
         }
     }
---
+++ RepositoryDesignPatternTests/BuyCommandTests.cs
@@ -0,0 +1,108 @@
+using System;
+using Moq;
+using RepositoryDesignPattern;
+using RepositoryDesignPattern.Commands;
+using RepositoryDesignPattern.Context;
+using Xunit;
+
+namespace RepositoryDesignPatternTests
+{
+    public class BuyCommandTests
+    {
+        [Fact]
+        public void CanExecute_ProductQuantityMoreThan0_True()
+        {
+            //Arrange
+            var product = new Product { Quantity = 1 };
+
+            var repositoryMock = new Mock<IRepository<Product>>(MockBehavior.Strict);
+            repositoryMock.Setup(rm => rm.Get(product.ProductId)).Returns(product);
+
+            var buyCommand = new BuyCommand(repositoryMock.Object, product.ProductId);
+
+            //Act
+            var actualResult = buyCommand.CanExecute();
+
+            //Assert
+            Assert.True(actualResult);
+        }
+
+        [Fact]
+        public void CanExecute_ProductQuantityEquals0_False()
+        {
+            //Arrange
+            var product = new Product { Quantity = 0 };
+
+            var repositoryMock = new Mock<IRepository<Product>>(MockBehavior.Strict);
+            repositoryMock.Setup(rm => rm.Get(product.ProductId)).Returns(product);
+
+            var buyCommand = new BuyCommand(repositoryMock.Object, product.ProductId);
+
+            //Act
+            var actualResult = buyCommand.CanExecute();
+
+            //Assert
+            Assert.False(actualResult);
+        }
+
+        [Fact]
+        public void Execute_ProductQuantityDecreasedFrom2To1_True()
+        {
+            //Arrange
+            var product = new Product { Quantity = 2 };
+
+            var repositoryMock = new Mock<IRepository<Product>>(MockBehavior.Strict);
+            repositoryMock.Setup(rm => rm.Get(It.IsAny<Guid>())).Returns(product);
+            repositoryMock.Setup(rm => rm.Update(It.IsAny<Product>())).Returns(product);
+            repositoryMock.Setup(rm => rm.SaveChanges());
+
+            var buyCommand = new BuyCommand(repositoryMock.Object, product.ProductId);
+
+            //Act
+            buyCommand.Execute();
+
+            //Assert
+            Assert.Equal(1, product.Quantity);
+        }
+
+        [Fact]
+        public void Execute_UpdateCalledOnce_True()
+        {
+            //Arrange
+            var product = new Product { Quantity = 2 };
+
+            var repositoryMock = new Mock<IRepository<Product>>(MockBehavior.Strict);
+            repositoryMock.Setup(rm => rm.Get(It.IsAny<Guid>())).Returns(product);
+            repositoryMock.Setup(rm => rm.Update(It.IsAny<Product>())).Returns(product);
+            repositoryMock.Setup(rm => rm.SaveChanges());
+
+            var buyCommand = new BuyCommand(repositoryMock.Object, product.ProductId);
+
+            //Act
+            buyCommand.Execute();
+
+            //Assert
+            repositoryMock.Verify(rm => rm.Update(It.IsAny<Product>()), Times.Once);
+        }
+
+        [Fact]
+        public void Execute_SaveChangesCalledOnce_True()
+        {
+            //Arrange
+            var product = new Product { Quantity = 2 };
+
+            var repositoryMock = new Mock<IRepository<Product>>(MockBehavior.Strict);
+            repositoryMock.Setup(rm => rm.Get(It.IsAny<Guid>())).Returns(product);
+            repositoryMock.Setup(rm => rm.Update(It.IsAny<Product>())).Returns(product);
+            repositoryMock.Setup(rm => rm.SaveChanges());
+
+            var buyCommand = new BuyCommand(repositoryMock.Object, product.ProductId);
+
+            //Act
+            buyCommand.Execute();
+
+            //Assert
+            repositoryMock.Verify(rm => rm.SaveChanges(), Times.Once);
+        }
+    }
+}
