f1800a08547ec6dc1ba9bb217fb42698f21909e1
Samuel Zigo <SZigo@moravia.com>
2022-04-19 13:58:06 +0200

    Adding unit tests using Boiler template.

--- RepositoryPattern.Tests/Commands/BuyCommandTests.cs
+++ RepositoryPattern.Tests/Commands/BuyCommandTests.cs
@@ -3,54 +3,77 @@ using NUnit.Framework;
 using RepositoryPattern;
 using RepositoryPattern.Commands;
 using RepositoryPattern.Context;
+using System;
 
 namespace RepositoryPattern.Tests.Commands
 {
     [TestFixture]
     public class BuyCommandTests : TestsBase
     {
-
-        private Mock<Product> mockProduct;
+        private Product product;
         private Mock<IRepository<Product>> mockRepository;
 
         [SetUp]
         public void SetUp()
         {
-            mockProduct = MockRepo.Create<Product>();
+            product = new Product();
             mockRepository = MockRepo.Create<IRepository<Product>>();
         }
 
         private BuyCommand CreateBuyCommand()
         {
             return new BuyCommand(
-                mockProduct.Object,
+                product,
                 mockRepository.Object);
         }
 
-        [Test]
-        public void CanExecute_StateUnderTest_ExpectedBehavior()
+        [TestCase(1,true)]
+        [TestCase(0, false)]
+        public void CanExecute_SetQuantity_ReturnsExpected(int quantity, bool expectedResult)
         {
             // Arrange
+            product.Quantity = quantity;
+            mockRepository.Setup(prm => prm.Get(It.IsAny<Guid>())).Returns(product);
+            
             var buyCommand = CreateBuyCommand();
 
             // Act
             var result = buyCommand.CanExecute();
 
             // Assert
-            Assert.Fail();
+            Assert.AreEqual(expectedResult,result);
         }
 
         [Test]
-        public void Execute_StateUnderTest_ExpectedBehavior()
+        public void Execute_QuantityAllowingDecretion_UpdateHasBeenCalled()
         {
             // Arrange
+            product.Quantity = 10;
             var buyCommand = CreateBuyCommand();
+            mockRepository.Setup(prm => prm.Get(It.IsAny<Guid>())).Returns(product);
+            mockRepository.Setup(prm => prm.Update(It.IsAny<Product>())).Returns(product);
 
             // Act
             buyCommand.Execute();
 
             // Assert
-            Assert.Fail();
+            mockRepository.Verify(prm => prm.Update(It.IsAny<Product>()), Times.Once);
+        }
+
+        [Test]
+        public void Execute_QuantityAllowingDecretion_QuantityDecresed()
+        {
+            // Arrange
+            product.Quantity = 10;
+            var buyCommand = CreateBuyCommand();
+            mockRepository.Setup(prm => prm.Get(It.IsAny<Guid>())).Returns(product);
+            mockRepository.Setup(prm => prm.Update(It.IsAny<Product>())).Returns(product);
+
+            // Act
+            buyCommand.Execute();
+
+            // Assert
+            Assert.AreEqual(9, product.Quantity);
         }
 
         [Test]
