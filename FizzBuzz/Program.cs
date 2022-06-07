99ddfe68071609d68c932eeef61e03683be5a1a2
Samuel Zigo <SZigo@moravia.com>
2022-06-07 16:29:28 +0200

    Finish FizzBuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FizzBuzz;
+//try
+//{
+//    var buzzGenerator = new FizzBuzzGenerator();
+//    Console.Write("Please provide number: ");
+//    var input = Console.ReadLine();
+//    Console.WriteLine(buzzGenerator.GetSequence(input));
+//} catch (Exception ex)
+//{
+//    Console.WriteLine(ex.ToString());
+//}
+
+
+var buzzGenerator = new FizzBuzzGenerator();
+Console.WriteLine("starting string joins");
+var start = DateTime.Now.Ticks;
+buzzGenerator.GetSequenceStringJoin("1");
+var end = DateTime.Now.Ticks;
+Console.WriteLine($"Total time for execution {end - start}");
+Console.WriteLine("_______________________________________");
+Console.WriteLine("starting string builder");
+start = DateTime.Now.Ticks;
+buzzGenerator.GetSequenceStringBuilder("1");
+end = DateTime.Now.Ticks;
+Console.WriteLine($"Total time for execution {end - start}");
+Console.WriteLine("_______________________________________");
+Console.WriteLine("starting strings");
+start = DateTime.Now.Ticks;
+buzzGenerator.GetSequenceStrings("1");
+end = DateTime.Now.Ticks;
+Console.WriteLine($"Total time for execution {end - start}");
+Console.WriteLine("_______________________________________");
