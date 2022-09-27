d4344226fb4c163f919e74a303c12e75568c9606
Filip Mucha <filip.mucha@rws.com>
2022-09-27 09:59:08 +0200

    added Product class

---
+++ RepositoryPattern/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace RepositoryPattern
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public Guid ProductId { get; private set; }
+
+        public Product()
+        {
+            ProductId = Guid.NewGuid();
+        }
+
+    }
+}
