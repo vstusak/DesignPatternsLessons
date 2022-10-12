b775478a30616d1ee002b53ec2ff7fd7af8ab66e
Milan Karasek <milan.karasek@rws.com>
2022-10-12 11:58:57 +0200

    Composite Pattern WIP

---
+++ CompositePattern/IFolder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    public interface IFolder
+    {
+        void AddItem(ISystemItem item);
+        void RemoveItem(string Name);
+    }
+}
---
+++ CompositePattern/MyFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    public class MyFile : ISystemItem
+    {
+        public string Name { get; }
+        private readonly int _size;
+
+        public MyFile(string name, int size)
+        {
+            Name = name;
+            _size = size;
+        }
+
+        public int GetSize()
+        {
+            return _size;
+        }
+    }
+}
