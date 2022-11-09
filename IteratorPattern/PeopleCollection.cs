e9389dc9a5e8006da9196d3f324bdfa3430a2218
Tomáš Fulajtár <tomas.fulajtar@rws.com>
2022-11-09 12:00:03 +0100

    Iterator pattern

---
+++ IteratorPattern/PeopleCollection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern
+{
+    public class PeopleCollection:List<Person>, IPeopleCollection
+    {
+        public IPeopleIterator CreateIterator()
+        {
+            return new PeopleIterator(this);
+        }
+    }
+
+    public class PeopleIterator : IPeopleIterator
+    {
+        private readonly PeopleCollection _people;
+        private int _currentPosition =0;
+
+        public PeopleIterator(PeopleCollection people)
+        {
+            _people = people;
+        }
+        public bool AtEnd()
+        {
+            return _currentPosition+1 >= _people.Count;
+        }
+
+        public void Next()
+        {
+            if (!AtEnd())
+            {
+                _currentPosition++;
+            }
+        }
+
+        public Person Current()
+        {
+            return _people[_currentPosition];
+        }
+    }
+
+    public interface IPeopleCollection
+    {
+        IPeopleIterator CreateIterator();
+    }
+
+    public interface IPeopleIterator
+    {
+        bool AtEnd();
+        void Next();
+        Person Current();
+    }
+}
---
+++ IteratorPattern/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+using IteratorPattern;
+
+var people = new PeopleCollection();
+
+people.Add(new Person(){Name="Milan",Age = 666 });
+people.Add(new Person() { Name = "Petr Jankovsky", Age = 33 });
+people.Add(new Person() { Name = "Petr Pucero", Age = 22 });
+people.Add(new Person() { Name = "Josef", Age = 32 });
+people.Add(new Person() { Name = "Tomas", Age = 11 });
+people.Add(new Person() { Name = "Vlada", Age = 55 });
+
+foreach (var person in people)
+{
+    Console.WriteLine(person.Name);
+}
+
+for (int i = 0; i < people.Count; i++)
+{
+    Console.WriteLine(people[i].Name);
+}
+
+var iterator =  people.CreateIterator();
+
+Console.WriteLine("Own Iterator ---");
+
+//TODO: eliminate AtEnd, and replace with Next/ Current methods
+while (!iterator.AtEnd())
+{
+    var person = iterator.Current();
+    Console.WriteLine(person.Name);
+    iterator.Next();
+}
+
+Console.WriteLine("After At End  Iterator ---");
+Console.WriteLine(iterator.Current().Name);
