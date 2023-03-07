a41aa003971fb9ec69e1cc68fb18f0186eca4266
Michaela Vitula <MVitula@moravia.com>
2023-03-07 10:00:42 +0100

    finished Singleton pattern add Lock

--- SingletonDesignPattern/FileAccessSingleton.cs
+++ SingletonDesignPattern/FileAccessSingleton.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,13 +10,22 @@ namespace SingletonDesignPattern
     public class FileAccessSingleton
     {
         private static FileAccessSingleton _instance;
+        private static readonly object _lockObject = new Object();
+
+        private FileAccessSingleton()
+        {
+        }
 
         public static FileAccessSingleton GetInstance()
         {
-            if (_instance == null)
+            lock (_lockObject)
             {
-                _instance = new FileAccessSingleton();
+                if (_instance == null)
+                {
+                    _instance = new FileAccessSingleton();
+                }
             }
+
             return _instance;
         }
 
--- SingletonDesignPattern/Program.cs
+++ SingletonDesignPattern/Program.cs
@@ -2,3 +2,8 @@
 
 var i = FileAccessSingleton.GetInstance();
 var j = FileAccessSingleton.GetInstance();
+Console.WriteLine(object.ReferenceEquals(i, j));
+
+var k = FileAccessSingleton.GetInstance();
+Console.WriteLine(object.ReferenceEquals(i,k));
+
