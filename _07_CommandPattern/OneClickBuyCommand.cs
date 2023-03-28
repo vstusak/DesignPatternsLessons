a89e766f70c29e059f23f13e6507c610bae1159c
Marek Vesely <MVesely@moravia.com>
2023-03-28 10:14:38 +0200

    done - Invoke, OrderNotificator todo - refactor, add product stock check

--- _07_CommandPattern/OneClickBuyCommand.cs
+++ _07_CommandPattern/OneClickBuyCommand.cs
@@ -2,16 +2,35 @@
 {
     private int _productId;
     private int _count;
+    private List<Product> _products;
 
     public OneClickBuyCommand(int productId, int count)
     {
         _productId = productId;
         _count = count;
+        _products = new List<Product>
+        {
+            new Product() {ProductId = 1, Amount = 5, Name = "Bodkociarka" },
+            new Product() {ProductId = 2, Amount = 10, Name = "Plafon" },
+            new Product() {ProductId = 3, Amount = 8, Name = "Cucoriedka"},
+        };
     }
 
     public void Invoke()
     {
-        //here some work will happen
-        throw new NotImplementedException();
+        var product = _products.Single(product => product.ProductId == _productId);
+        product.Amount -= _count;
+        var message = $"Koupil jsi {product.Name} v počtu {_count} kusů. Na skladě zůstalo {product.Amount} kusů.";
+        var notification = new OrderNotificator();
+        notification.SendNotification(message);
+    }
+
+}
+
+public class OrderNotificator // TODO zmenit na interface
+{
+    public void SendNotification(string notificationMessage)
+    {
+        Console.WriteLine(notificationMessage);
     }
 }
---
+++ _07_CommandPattern/Product.cs
@@ -0,0 +1,6 @@
+﻿public class Product
+{
+    public int ProductId { get; set; }
+    public string Name { get; set; }
+    public int Amount { get; set; }
+}
