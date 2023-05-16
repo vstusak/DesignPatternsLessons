1a4394322caa04a997cab6ba12209a02424a2134
Marek Vesely <MVesely@moravia.com>
2023-05-16 09:58:57 +0200

    Changed List to Stack (improved Undo method)

--- _07_CommandPattern/CommandManager.cs
+++ _07_CommandPattern/CommandManager.cs
@@ -1,29 +1,36 @@
 ﻿public class CommandManager
 {
-    private readonly List<ICommand> _commands = new();
+    private readonly Stack<ICommand> _commands = new();
 
     public void RunCommand(ICommand command)
     {
         if (command.CanInvoke())
         {
             command.Invoke();
-            _commands.Add(command);
+            _commands.Push(command);
         }
         else
         {
             command.ValidationMessage();
         }
+
     }
 
     public void Undo()
     {
-        var lastCommand = _commands.LastOrDefault();
-        
-        if (lastCommand != null) 
+        try
         {
+            var lastCommand = _commands.Pop();
             lastCommand.Undo();
-            //TODO - add remove
         }
+        catch (Exception e)
+        {
+            //Console.WriteLine(e);
+            Console.WriteLine("There are no commands to undo.");
+            //throw e;
+            //TODO probrat poradne exception handling
+        }
+
     }
 
     public void Redo()
