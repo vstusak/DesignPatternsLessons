3dec57fc7929106f118453be6bf4398e2f207f2f
Richard Ondrejka <richard.ondrejka@rws.com>
2023-07-20 13:57:01 +0200

    ASP Validations

---
+++ RegistrationAppProperValidations/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationAppProperValidations.Models;
+
+public class Person
+{
+    [DisplayName("First name")]
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public string Surename { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Nino { get; set; }
+}
---
+++ RegistrationAppProperValidations/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RegistrationAppProperValidations.Models;
+
+namespace RegistrationAppProperValidations.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public Person Person { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+}
