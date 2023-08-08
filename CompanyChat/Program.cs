f4e585971d29174160b6af68b7ef955dfa61eae4
Samuel Zigo <SZigo@moravia.com>
2023-08-08 13:58:44 +0200

    Kick off for company chat project

---
+++ CompanyChat/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Mediator pattern could be useful for central managing of references. 
+//OR keep references in each object???
+//TODO: Make decision.
+Console.WriteLine("Hello, World!");
