12bc1ee766c14e96b1288b991795aa718612fe12
Milan Karasek <milan.karasek@rws.com>
2023-10-04 11:57:57 +0200

    Flyweight WIP

---
+++ FlyweightPattern/Particle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyweightPattern
+{
+    public class Particle
+    {
+        public Point Coords { get; set; }
+        public int Vector { get; set; }
+        public int Speed { get; set; }
+        public Color Color { get; set; }
+        public byte[] Sprite { get; set; }
+
+        public Particle()
+        {
+            Sprite = new byte[2048 * 2048];
+        }
+
+        public override string ToString()
+        {
+            return $"{Coords.X}, {Coords.Y}";
+        }
+    }
+
+}
--- FlyweightPattern/Program.cs
+++ FlyweightPattern/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FlyweightPattern;
+
+Console.ReadLine();
+
+Console.WriteLine("Flyweight!");
+
+var originalSize = GC.GetTotalMemory(false);
+Console.WriteLine("Before: " + originalSize.ToString());
+
+var particle = new Particle();
+
+Console.WriteLine("After:  " + GC.GetTotalMemory(false).ToString());
+Console.WriteLine("Diff:   " + (GC.GetTotalMemory(false) - originalSize).ToString());
+
+
+Console.ReadLine();
+
+Console.WriteLine(particle.ToString());
