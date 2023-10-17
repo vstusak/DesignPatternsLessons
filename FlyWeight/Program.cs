ef5983b9a4448a3fb7d5d712750007193c76c751
Samuel Zigo <SZigo@moravia.com>
2023-10-17 13:58:17 +0200

    Kicking off Flyweight

---
+++ FlyWeight/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ReadLine();
+Console.WriteLine("Hello, World!");
+List<Blob> blobs = new List<Blob>();
+blobs.Add(new Blob());
+blobs.Add(new Blob());
+blobs.Add(new Blob());
+blobs.Add(new Blob());
+blobs.Add(new Blob());
+blobs.Add(new Blob());
+Console.ReadLine();
+
+class Blob
+{
+    public Blob()
+    {
+        Name = Guid.NewGuid().ToString();
+        Bytes = new byte[2048*2048];
+    }
+    string Name { get; set; }
+    Byte[] Bytes { get; set; }
+}
