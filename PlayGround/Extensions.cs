b7a1d2694c2ae87b63306b210255d20e13f83e52
Petr Kosina <PKosina@moravia.com>
2024-01-02 10:02:19 +0100

    New Class

---
+++ PlayGround/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayGround
+{
+    public static class Extensions
+    {
+        public static string ToLowerCase(this string str)
+        {
+            return str.ToLower();
+        }
+
+        public static int CountHigherCases(this string str)
+        {
+            int higherCasesCount = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsUpper(str[i]))
+                {
+                    higherCasesCount++;
+                }
+            }
+            return higherCasesCount;
+        }
+    }
+}
---
+++ PlayGround/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PlayGround;
+
+Console.WriteLine("Hello, World!");
+
+string str = "Hello World!";
+Console.WriteLine(str.ToLower());
+
+//var ext = new Extensions();
+//Console.WriteLine(ext.ToLowerCase(str));
+
+str.ToLowerCase();
+
+Console.WriteLine(str.CountHigherCases());
