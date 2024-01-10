2e1ed91f2061ac6f5c36d63286ea4574559c3e07
Milan Karasek <milan.karasek@rws.com>
2024-01-10 13:01:09 +0100

    .net 8 new features

---
+++ Net8newFeatures/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+//TODO: Continue on 'Disable reflection-based default' (https://learn.microsoft.com/en-us/dotnet/core/whats-new/dotnet-8)
+
+
+CustomerInfo customer =
+    JsonSerializer.Deserialize<CustomerInfo>("""
+                                             {"Names":["John Doe"],"Company":{"Name":"Contoso"}}
+                                             """)!;
+
+Console.WriteLine(JsonSerializer.Serialize(customer));
+
+class CompanyInfo
+{
+    public string Name { get; set; }
+    //public required string Name { get; set; }
+    public string? PhoneNumber { get; set; }
+}
+
+[JsonObjectCreationHandling(JsonObjectCreationHandling.Populate)]
+class CustomerInfo
+{
+    // Both of these properties are read-only.
+    public List<string> Names { get; } = new();
+    public CompanyInfo Company { get; } = new()
+    {
+        Name = "N/A", PhoneNumber = "N/A"
+    };
+}
---
+++ Net8newFeatures/derivedInterfaces.cs
@@ -0,0 +1,28 @@
+﻿//using System.Text.Json;
+
+
+
+//DerivedImplement value = new DerivedImplement { Base = 0, Derived = 1 };
+//value.Type = "xxx";
+
+//Console.WriteLine(JsonSerializer.Serialize((IDerived)value));
+//// {"Base":0,"Derived":1}
+
+
+
+//public interface IBase
+//{
+//    public int Base { get; set; }
+//}
+
+//public interface IDerived : IBase
+//{
+//    public int Derived { get; set; }
+//}
+
+//public class DerivedImplement : IDerived
+//{
+//    public int Base { get; set; }
+//    public int Derived { get; set; }
+//    public string Type { get; set; }
+//}
