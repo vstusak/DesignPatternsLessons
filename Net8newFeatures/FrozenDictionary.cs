c3c16f67d45f9c36d3ed7fc085d4974b45c2f718
Petr Pucek <ppucek@moravia.com>
2024-01-31 12:03:27 +0100

    Frozen dictionary - https://learn.microsoft.com/en-us/dotnet/core/whats-new/dotnet-8#stream-based-zipfile-methods

---
+++ Net8newFeatures/FrozenDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Frozen;
+using System.Diagnostics;
+using Net8newFeatures;
+
+var dictionary = new Dictionary<string, object>();
+var source = new []{ "jedna", "dva", "tri", "ctyri", "pet" };
+var searchedKey = "12330";
+
+var stopWatch = new Stopwatch();
+var stopWatch2 = new Stopwatch();
+
+for (int i = 0; i < 10000; i++)
+{
+    dictionary[i.ToString()] = Random.Shared.GetItems(source, 1);
+}
+
+var frozen = dictionary.ToFrozenDictionary();
+
+stopWatch.Start();
+//var search = dictionary[searchedKey];
+stopWatch.Stop();
+
+Console.WriteLine($"Dictionary: {stopWatch.ElapsedTicks}");
+
+stopWatch2.Start();
+//var search2 = frozen[searchedKey];
+stopWatch2.Stop();
+
+Console.WriteLine($"Frozen: {stopWatch2.ElapsedTicks}");
+
+stopWatch.Reset();
+stopWatch2.Reset();
+
+stopWatch.Start();
+dictionary.TryGetValue(searchedKey, out object setting);
+stopWatch.Stop();
+
+Console.WriteLine($"Dictionary: {stopWatch.ElapsedTicks}");
+
+stopWatch2.Start();
+frozen.TryGetValue(searchedKey, out object setting2);
+stopWatch2.Stop();
+
+Console.WriteLine($"Frozen: {stopWatch2.ElapsedTicks}");
+
