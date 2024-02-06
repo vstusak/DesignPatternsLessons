92f71bba8992fe11aa0c15dd4ffcb294048bc1e3
Radim Jilek <RJilek@moravia.com>
2024-02-06 13:58:23 +0100

    Play with .NET 8.0

---
+++ DotNet_8_playground/InterfaceInheritanceSerialization.cs
@@ -0,0 +1,22 @@
+﻿//using System.Text.Json;
+
+//IDerived value = new DerivedImplement { Base = 0, Derived = 1 };
+//var serialize = JsonSerializer.Serialize(value);
+//Console.WriteLine(serialize);
+//// {"Base":0,"Derived":1}
+
+//public interface IBase
+//{
+//    public int Base { get; set; }
+//}
+
+//public interface IDerived : IBase
+//{
+//    public int Derived { get; set; }
+//}
+
+//public class DerivedImplement : IDerived
+//{
+//    public int Base { get; set; }
+//    public int Derived { get; set; }
+//}
---
+++ DotNet_8_playground/PopulateReadOnlyProperties.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+var customer =
+    JsonSerializer.Deserialize<CustomerInfo>("""
+                                             {"Names":["John Doe"],"Company":{"Name":"Contoso"}}
+                                             """)!;
+
+Console.WriteLine(JsonSerializer.Serialize(customer));
+
+internal class CompanyInfo
+{
+    public required string Name { get; set; }
+    public string? PhoneNumber { get; set; }
+}
+
+[JsonObjectCreationHandling(JsonObjectCreationHandling.Populate)]
+internal class CustomerInfo
+{
+    // Both of these properties are read-only.
+    public List<string> Names { get; } = new();
+
+    public CompanyInfo Company { get; } = new()
+    {
+        Name = "N/A",
+        PhoneNumber = "N/A"
+    };
+}
