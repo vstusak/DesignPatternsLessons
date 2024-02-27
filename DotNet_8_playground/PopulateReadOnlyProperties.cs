f047897742f61c73535d067de868e75b391a9177
Richard Ondrejka <richard.ondrejka@rws.com>
2024-02-27 13:59:58 +0100

    news in dotnet 8

--- DotNet_8_playground/PopulateReadOnlyProperties.cs
+++ DotNet_8_playground/PopulateReadOnlyProperties.cs
@@ -1,27 +1,28 @@
-﻿using System.Text.Json;
+﻿//using System.Text.Json;
+//using System.Text.Json.Serialization;
 
-var customer =
-    JsonSerializer.Deserialize<CustomerInfo>("""
-                                             {"Names":["John Doe"],"Company":{"Name":"Contoso"}}
-                                             """)!;
+//var customer =
+//    JsonSerializer.Deserialize<CustomerInfo>("""
+//                                             {"Names":["John Doe"],"Company":{"Name":"Contoso"}}
+//                                             """)!;
 
-Console.WriteLine(JsonSerializer.Serialize(customer));
+//Console.WriteLine(JsonSerializer.Serialize(customer));
 
-internal class CompanyInfo
-{
-    public required string Name { get; set; }
-    public string? PhoneNumber { get; set; }
-}
+//internal class CompanyInfo
+//{
+//    public required string Name { get; set; }
+//    public string? PhoneNumber { get; set; }
+//}
 
-[JsonObjectCreationHandling(JsonObjectCreationHandling.Populate)]
-internal class CustomerInfo
-{
-    // Both of these properties are read-only.
-    public List<string> Names { get; } = new();
+//[JsonObjectCreationHandling(JsonObjectCreationHandling.Populate)]
+//internal class CustomerInfo
+//{
+//    // Both of these properties are read-only.
+//    public List<string> Names { get; } = new();
 
-    public CompanyInfo Company { get; } = new()
-    {
-        Name = "N/A",
-        PhoneNumber = "N/A"
-    };
-}
+//    public CompanyInfo Company { get; } = new()
+//    {
+//        Name = "N/A",
+//        PhoneNumber = "N/A"
+//    };
+//}
