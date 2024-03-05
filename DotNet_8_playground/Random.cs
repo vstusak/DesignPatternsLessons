0abcee326768546cd192d584cc28724491fe5236
Samuel Zigo <SZigo@moravia.com>
2024-03-05 13:59:51 +0100

    TimeProvider + Random TODO: shuffle

---
+++ DotNet_8_playground/Random.cs
@@ -0,0 +1,18 @@
+﻿ReadOnlySpan<Button> s_allButtons = new[]
+{
+    Button.Red,
+    Button.Green,
+    Button.Blue,
+    Button.Yellow,
+};
+
+Button[] thisRound = Random.Shared.GetItems(s_allButtons, 31);
+
+foreach (Button button in thisRound)
+{
+    Console.WriteLine(button);
+}
+public enum Button
+{
+    Red,Green,Blue,Yellow
+}
