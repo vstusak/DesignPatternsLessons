bfc2b977d3ba077837ffe3f68d216bf51738eaa9
Petr Kosina <PKosina@moravia.com>
2024-03-22 14:01:51 +0100

    Add Person class

---
+++ PlayGround/Person.cs
@@ -0,0 +1,6 @@
+﻿public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public DateTime Birth { get; set; }
+}
--- PlayGround/Program.cs
+++ PlayGround/Program.cs
@@ -1,15 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using PlayGround;
+using System.Xml.Linq;
+#region EXTENSION METHODS
+//Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello, World!");
+//string str = "Hello World!";
+//Console.WriteLine(str.ToLower());
 
-string str = "Hello World!";
-Console.WriteLine(str.ToLower());
+////var ext = new Extensions();
+////Console.WriteLine(ext.ToLowerCase(str));
 
-//var ext = new Extensions();
-//Console.WriteLine(ext.ToLowerCase(str));
+//str.ToLowerCase();
 
-str.ToLowerCase();
+//Console.WriteLine(str.CountHigherCases());
+#endregion
 
-Console.WriteLine(str.CountHigherCases());
+
+var pepa = new Person()
+{
+    Age = 5,
+    Birth = new DateTime(2019,01,01),
+    Name = "Pepa"
+};
+var jarda = new Person()
+{
+    Age = 7,
+    Birth = new DateTime(2017, 02, 12),
+    Name = "Jarda"
+};
+
+var lenka = new Person()
+{
+    Age = 10,
+    Birth = new DateTime(2013, 07, 30),
+    Name = "Lenka"
+};
+
+var people = new List<Person>();
+people.Add(pepa);
+people.Add(jarda);
+people.Add(lenka);
+
+var names = people.Select(x => x.Name).ToList();
