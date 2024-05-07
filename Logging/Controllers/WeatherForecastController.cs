e86c6133902b8cd5e2f9d40e382dc327ea89474c
Samuel Zigo <SZigo@moravia.com>
2024-05-07 14:00:32 +0200

    adding pokemon data

--- Logging/Controllers/WeatherForecastController.cs
+++ Logging/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace Logging.Controllers
+namespace Logging.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- Logging/WeatherForecast.cs
+++ Logging/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace Logging
+namespace Logging.Api
 {
     public class WeatherForecast
     {
