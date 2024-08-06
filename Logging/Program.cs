204370013b69e59cfbffac1aa1c0ce8d0d24a59b
Radim Jilek <RJilek@moravia.com>
2024-08-06 13:56:20 +0200

    File logger

--- Logging/Program.cs
+++ Logging/Program.cs
@@ -1,14 +1,15 @@
 using Logging.Data;
 using Logging.Domain;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<PokemonContext>();
 
 // ToDo apply filters alsop from other places than appsettings
-
 // logging
-//builder.Logging.AddProvider();
+//builder.Logging.AddFilter("DataAccessLayer", LogLevel.Information);
+builder.Logging.AddProvider(new FileLoggerProvider());
 
 // Add services to the container.
 builder.Services.AddScoped<IPokemonProvider, PokemonProvider>();
@@ -44,3 +45,47 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public class FileLoggerProvider : ILoggerProvider
+{
+    public void Dispose()
+    {
+        return;
+    }
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        return new FileLogger(categoryName);
+    }
+}
+
+public class FileLogger : ILogger
+{
+    private readonly string _categoryName;
+    private readonly FileStream _fileStream;
+
+    public FileLogger(string categoryName)
+    {
+        _categoryName = categoryName;
+        _fileStream = new FileStream(LogFilePath, FileMode.OpenOrCreate);
+    }
+
+    private const string LogFilePath = "";
+
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+    {
+        return null;
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return true;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception,
+        Func<TState, Exception?, string> formatter)
+    {
+        var message = formatter(state, exception);
+        // @TODO Finish FileLogger and try
+    }
+}
