679aa48ec21ae2c104fdd28c0dece6835deb8937
Petr Pucek <ppucek@moravia.com>
2024-09-11 11:58:01 +0200

    Added todos

---
+++ todos.cs
@@ -0,0 +1,5 @@
+﻿// Finish index.cshtml - CRUD operations on products
+// Logging.API - create randomly failing endpoint
+// Logging.WebApp - use Polly library to secure communication with failing API
+// Test coverage - increase from 0% to 20%
+// Move data from file to DB - docker
