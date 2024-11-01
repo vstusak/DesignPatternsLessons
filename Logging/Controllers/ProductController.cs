63366a4bb527c945343022b75568c73706cb97bc
Tomas Fulajtar <tomas.fulajtar@rws.com>
2024-11-01 11:55:10 +0100

    FailingEndpoint.

--- Logging/Controllers/ProductController.cs
+++ Logging/Controllers/ProductController.cs
@@ -44,6 +44,33 @@ namespace Logging.Api.Controllers
             return Ok(product);
         }
 
+        [HttpGet("Fail/{id:int}")]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status501NotImplemented)]
+        public IActionResult GetWithFail(int id)
+        {
+            _logger.LogDebug($"Get product with '{id}' id");
+
+            var rand = Random.Shared;
+            
+            if (rand.Next(1, 10) > 5)
+            {
+                var product = _productProvider.GetProduct(id);
+
+                if (product == null)
+                {
+                    _logger.LogWarning($"Cannot find product with '{id}' id");
+                    return NotFound();
+                }
+                return Ok(product);
+            }
+
+            throw new NotImplementedException();
+        }
+
+
         [HttpDelete("{id:int}")]
         public IActionResult Delete(int id)
         {
--- Logging/todos.cs
+++ Logging/todos.cs
@@ -1,8 +1,9 @@
-﻿// --Resolve logging to console for delete (we can't see anything) - on filters warning and higher was allowed
-// ToDo: Resolve Edit - add submit and save new data. We are api controller now. Continue with repo
-// Finish index.cshtml - CRUD operations on products
-//check log for the delete operation.
-// Logging.API - create randomly failing endpoint
+﻿// --Resolve logging to console for delete (we can't see anything) - on filters warning and higher was allowed -Done
+// Resolve Edit - add submit and save new data. We are api controller now. Continue with repo - Done
+// Finish index.cshtml - CRUD operations on products - Done
+// check log for the delete operation. - Done
+// Logging.API - create randomly failing endpoint 
+// TODO - WebApp : create details of product by Id. Get it from failing endpoint
 // Logging.WebApp - use Polly library to secure communication with failing API
 // Test coverage - increase from 0% to 20%
 // Move data from file to DB - docker
