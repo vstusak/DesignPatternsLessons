1ca87ced2e16fd2c830c32a00e5512e90165c44a
Jiri Pazdera <JPazdera@moravia.com>
2025-01-23 14:01:21 +0100

    AbstractFactory initial commit

---
+++ FastAndFurious/AbstractFactory/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Card
+{
+    public int CardId { get; set; }
+    public CardType Type { get; init; }
+    public int AccountId { get; set; }
+    public string HolderId { get; set; }
+}
+
+public class Account
+{
+    public int AccountId { get; set; }
+    public AccountType Type { get; init; }
+    public string HolderId { get; set; }
+}
+
+public enum CardType
+{
+    Credit,
+    Debet
+}
+
+public enum AccountType
+{
+    Regular,
+    Savings,
+    Credit,
+    FeesFree
+}
