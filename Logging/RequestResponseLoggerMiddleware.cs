8ccd7e1da1c27ec6b52ebb7b54e2449c40658346
SZigo <SZigo@moravia.com>
2025-02-06 11:57:47 +0100

    Middleware updates

--- Logging/RequestResponseLoggerMiddleware.cs
+++ Logging/RequestResponseLoggerMiddleware.cs
@@ -1,16 +1,30 @@
-﻿namespace ProductStore.WebApi
+﻿using System.Net;
+
+namespace ProductStore.WebApi
 {
     public class RequestResponseLoggerMiddleware(RequestDelegate next, ILogger<RequestResponseLoggerMiddleware> logger)
     {
         public async Task InvokeAsync(HttpContext context)
         {
-            if (context.Request.Path.Value?.Contains("swagger") ?? false)
+            if (!context.Request.Path.Value?.Contains("swagger") ?? false)
             {
-                //TODO: add body to log message
                 logger.LogInformation($"{context.Request.Path.ToString()} {context.Request.Method}");
+
+                if (!context.Request.Path.Value?.Contains("Petr") ?? false)
+                {
+                    context.Response.StatusCode = 500;
+                    //TODO: Make some reasonable return value
+                }
+
+                var streamReader = new StreamReader(context.Request.Body);
+                string requestBody = await streamReader.ReadToEndAsync();
+                if (!string.IsNullOrEmpty(requestBody))
+                {
+                    logger.LogInformation($"Request body: {requestBody}");
+                }
             }
 
-            //TODO: stop processing if there is petr in request
+            
 
             await next(context);
 
