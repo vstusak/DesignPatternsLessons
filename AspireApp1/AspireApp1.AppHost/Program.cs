019f7ba994bbee09683798007bf84cf7731e4f74
Tomas Fulajtar <tomas.fulajtar@rws.com>
2025-04-10 11:59:31 +0200

    Aspire Project sample added. Redis disabled.

---
+++ AspireApp1/AspireApp1.AppHost/Program.cs
@@ -0,0 +1,14 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+//TODO cache var cache = builder.AddRedis("cache");
+
+var apiService = builder.AddProject<Projects.AspireApp1_ApiService>("apiservice");
+
+builder.AddProject<Projects.AspireApp1_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    // .WithReference(cache)
+    // .WaitFor(cache)
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.Build().Run();
