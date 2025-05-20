10ce0b43d49d615c93c18e1fad1e664b2b164788
Jiri Pazdera <JPazdera@moravia.com>
2025-05-20 12:25:08 +0200

    Initial commit

---
+++ FastAndFurious/RepositoryPattern/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern
+{
+    class ProductRepository : IRepository<Product>
+    {
+        private readonly WarehouseDbContext _warehouseDbContext;
+
+        public ProductRepository(WarehouseDbContext warehouseDbContext)
+        {
+            _warehouseDbContext = warehouseDbContext;
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Create(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
