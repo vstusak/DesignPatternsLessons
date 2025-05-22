2f077981f2509b1c53b1a4c1d8f1d3958fcc405a
Petr Pucek <ppucek@moravia.com>
2025-05-22 13:22:10 +0200

    Added dummy loader to Products Aspire Host

--- ProductStore.AspireHost/Program.cs
+++ ProductStore.AspireHost/Program.cs
@@ -1,10 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-//TODO: Finalize implementing Aspire on this project
+//TODO: Finalize implementing Aspire on this project ->
+//TODO: move data to database in docker, create docker initialization, setup docker to use local volume
 
+//TODO: use cache
 //var cache = builder.AddRedis("cache");
 
-var apiService = builder.AddProject<Projects.ProductStore_WebApi>("apiservice");
+var loader = builder.AddProject<Projects.ProductStore_Loader>("loader");
+
+var apiService = builder.AddProject<Projects.ProductStore_WebApi>("apiservice").WaitForCompletion(loader);
 
 builder.AddProject<Projects.ProductStore_WebApp>("webfrontend")
     .WithExternalHttpEndpoints()
@@ -12,6 +16,6 @@ builder.AddProject<Projects.ProductStore_WebApp>("webfrontend")
     //.WaitFor(cache)
     .WithReference(apiService)
     .WaitFor(apiService);
-    //TODO: Check how WaitFor is handeled (healthchecks, etc.)
+    //TODO: Check how WaitFor is handled (healthchecks, etc.)
 
 builder.Build().Run();
---
+++ ProductStore.Loader/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Starting loader...");
+
+Thread.Sleep(5000);
+
+Console.WriteLine("Loading done.");
