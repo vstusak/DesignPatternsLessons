3910709d23fb2ac08ef9d9a8c36801f6f519ee09
Jiri Pazdera <JPazdera@moravia.com>
2025-06-12 14:26:42 +0200

    Singleton

---
+++ FastAndFurious/SingletonPattern/FileLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class FileLogger
+    {
+
+        private static FileLogger _instance = null;
+
+        private readonly string _filePath = "log.txt";
+
+        private FileLogger()
+        {
+            
+        }
+        public static FileLogger GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new FileLogger();
+            }
+            return _instance;
+        }
+
+        public void LogToFile(string message)
+        {
+            using StreamWriter writer = new StreamWriter(new FileStream(_filePath, FileMode.OpenOrCreate));
+            writer.WriteLine(message);
+        }
+    }
+}
---
+++ FastAndFurious/SingletonPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using SingletonPattern;
+
+FileLogger.GetInstance().LogToFile("message");
+
+var instanceOne = FileLogger.GetInstance();
+var instanceTwo = FileLogger.GetInstance();
+
+var equality = Object.ReferenceEquals(instanceOne, instanceTwo);
+instanceOne.LogToFile(equality.ToString());
+instanceTwo.LogToFile(equality.ToString());
+
+// todo final file is fucked, please solve, bye
