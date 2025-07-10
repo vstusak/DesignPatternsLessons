1a6ca2160f551529610df7d72bb521f8c11a064e
Milan Karasek <milan.karasek@rws.com>
2025-07-10 15:02:25 +0200

    Aspire.Hosting.SqlServer problem on Docker

--- ProductStore.AspireHost/Program.cs
+++ ProductStore.AspireHost/Program.cs
@@ -1,10 +1,21 @@
 //using Aspire.Hosting.SqlServer;
+using Redis = Aspire.Hosting.Redis;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var sql = builder.AddSqlServer("sql")
-    .WithDataVolume()
-    .AddDatabase("sqldb");
+//var sql = builder.AddSqlServer("sql")
+//    .WithDataVolume()
+//    .AddDatabase("sqldb");
+
+//TODO: Tady Milan skoncil, password nelze vlozit (mlati se s Redisem?)
+var sql = builder
+        .AddSqlServer("sql")
+        .WithPassword(password: "password123")
+    //.WithDataBindMount(@"C:\SqlServer\Data", false)
+    //.WithEndpoint(port:6033, targetPort:1433, name:"ssms")
+    //.WithLifetime(ContainerLifetime.Persistent)
+    //.WithContainerName<SqlServerServerResource>("sqlserver")
+    ;
 
 //TODO: Finalize implementing Aspire on this project ->
 //TODO: move data to database in docker, create docker initialization, setup docker to use local volume
