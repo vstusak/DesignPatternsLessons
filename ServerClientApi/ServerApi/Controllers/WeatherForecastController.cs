90a3e3552d98717356b4ed8c42d46b1212913ea4
Radim Jilek <RJilek@moravia.com>
2025-07-28 12:51:29 +0200

    Add project for client implementation

--- ServerClientApi/ServerApi/Controllers/WeatherForecastController.cs
+++ ServerClientApi/ServerApi/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace ServerApi.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogError("Hello world");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
