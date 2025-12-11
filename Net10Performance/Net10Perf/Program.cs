b7bf6e637f25353896a79df20ea81361eba119a7
Milan Karasek <milan.karasek@rws.com>
2025-12-11 13:13:00 +0100

    Linq performance (First/Contains/Shuffle)

--- Net10Performance/Net10Perf/Program.cs
+++ Net10Performance/Net10Perf/Program.cs
@@ -15,13 +15,17 @@ using System.Runtime.InteropServices;
 
 // --== Useful code ==--
 
-const int Iters = 10_000_0;
+const int Iters = 10_000_00;
 //var values = Enumerable.Range(0, 100).ToList();
 //var values = new Stack<int>(Enumerable.Range(0, 100));
 //var values = new Queue<int>(Enumerable.Range(0, 100));
 //var values = new ConcurrentDictionary<int, int>(Enumerable.Range(0, 100).Select(e => new KeyValuePair<int, int>(e,e)));
 var values1 = new BitArray(1024, false);
 var values2 = new BitArray(1024, true);
+var values3 = Enumerable.Range(0, 100).Shuffle(); //.Reverse(); //.OrderBy(x => -x);
+
+Console.WriteLine(string.Join(" ", values3));
+
 
 Console.WriteLine("running...");
 
@@ -34,7 +38,8 @@ while (true)
     
     for (int i = 0; i < Iters; i++)
     {
-        Test(values1, values2);
+        //Test(values1, values2);
+        TestLinq(values3);
     }
 
     sw.Stop();
@@ -46,33 +51,42 @@ while (true)
 
 
 [MethodImpl(MethodImplOptions.NoInlining)] // prevent inlining to get more accurate measurements
-static long Test(BitArray values1, BitArray values2)
+static int TestLinq(IEnumerable<int> values)
 {
-#if NET10_0
-    return TensorPrimitives.HammingBitDistance<byte>(
-    CollectionsMarshal.AsBytes(values1),
-    CollectionsMarshal.AsBytes(values2));
-#else
-    //var stopWatch = Stopwatch.StartNew();
-    //stopWatch.Stop();
-    var distance = 0;
-
-    for (int i = 0; i < values1.Length; i++)
-    {
-        if(values1[i] != values2[i])
-        { 
-            distance++; 
-        }
-    }
-
-    return distance;
-#endif
-
-    //foreach(var i in values)
-    //{
-    //    distance += i.Value;
-    //}
-
-    //return distance;
+    return values.First();
+    //return values.Contains(35);
 }
+
+
+
+//[MethodImpl(MethodImplOptions.NoInlining)] // prevent inlining to get more accurate measurements
+//static long Test(BitArray values1, BitArray values2)
+//{
+//#if NET10_0
+//    return TensorPrimitives.HammingBitDistance<byte>(
+//    CollectionsMarshal.AsBytes(values1),
+//    CollectionsMarshal.AsBytes(values2));
+//#else
+//    //var stopWatch = Stopwatch.StartNew();
+//    //stopWatch.Stop();
+//    var distance = 0;
+
+//    for (int i = 0; i < values1.Length; i++)
+//    {
+//        if(values1[i] != values2[i])
+//        { 
+//            distance++; 
+//        }
+//    }
+
+//    return distance;
+//#endif
+
+//    //foreach(var i in values)
+//    //{
+//    //    distance += i.Value;
+//    //}
+
+//    //return distance;
+//}
 
