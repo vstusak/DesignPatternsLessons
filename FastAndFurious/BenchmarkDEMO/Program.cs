21d8d744f9bc74b0efafcf4d16c6a1c84217d13f
Jakub Horniak <JHorniak@moravia.com>
2025-12-18 09:58:29 +0100

    benchmarks demo

---
+++ FastAndFurious/BenchmarkDEMO/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+//var summary = BenchmarkRunner.Run<DotNET10LoopBenchmarks>();
+
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+
+[SimpleJob(RuntimeMoniker.Net10_0, baseline: true)]
+[SimpleJob(RuntimeMoniker.Net90)]
+[SimpleJob(RuntimeMoniker.Net48)]
+[RPlotExporter]
+public class DotNET10LoopBenchmarks
+{
+    [Benchmark]
+    public int ForEachIEnumerableSumBenchamrk()
+    {
+        return 0;
+    }
+}
+
+[SimpleJob(RuntimeMoniker.Net10_0, baseline: true)]
+[SimpleJob(RuntimeMoniker.Net90)]
+[SimpleJob(RuntimeMoniker.Net48)]
+[RPlotExporter]
+public class DotNET10LoopBenchmarks2
+{
+    [Benchmark]
+    public int ForEachIEnumerableSumBenchamrk()
+    {
+        return 0;
+    }
+}
---
+++ FastAndFurious/DotNET10Perf/UnitUnderPerformanceTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNET10Perf
+{
+    public class UnitUnderPerformanceTest
+    {
+        public int Test4(IEnumerable<int> values)
+        {
+            int summary = 0;
+
+            foreach (int item in values)
+            {
+                summary += item;
+            }
+
+            return summary;
+        }
+    }
+}
