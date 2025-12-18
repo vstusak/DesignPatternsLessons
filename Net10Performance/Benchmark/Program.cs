ba15caf7f8b1b8b133567a871325cef05d5fe614
Tomas Fulajtar <tomas.fulajtar@rws.com>
2025-12-18 13:27:01 +0100

    Benchmark package  + project added.

---
+++ Net10Performance/Benchmark/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+//var summary = BenchmarkRunner.Run<BenchmarkNet10LoopPerformance>();
+var summary = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+
+/*[SimpleJob(RuntimeMoniker.Net10_0)]
+[SimpleJob(RuntimeMoniker.Net90)]
+[SimpleJob(RuntimeMoniker.Net481)]
+*/
+[RPlotExporter]
+public class BenchmarkNet10LoopPerformance
+{
+    [Benchmark]
+    public int ForEachEnumerable()
+    {
+
+        return 0;
+    }
+    [Benchmark]
+    public int ForEachEnumerableTwo()
+    {
+
+        return 0;
+    }
+}
