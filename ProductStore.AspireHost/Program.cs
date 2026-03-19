d7b17e8a13fe62d3a8260c7d20c733c7d2001abf
Vladimir Stusak <vstusak@users.noreply.github.com>
2026-03-19 10:33:33 +0100

    unicode fix

--- ProductStore.AspireHost/Program.cs
+++ ProductStore.AspireHost/Program.cs
@@ -6,7 +6,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 //var sql = builder.AddSqlServer("sql")
 //  .WithDataVolume()
 //.AddDatabase("sqldb");
-var sqlPassword = builder.AddParameter("sql-password", "Sup�rS�cr�t2499");
+var sqlPassword = builder.AddParameter("sql-password", "Sup€rS€cret2499");
 var sqlserver = builder.AddSqlServer("sqlserver", password:sqlPassword)
     .WithImageTag("2025-latest")
     .WithDataVolume()
