c8641c54f14bf53f0433c6eb750f124c6a15cbf1
Pavel Dolezal <PDolezal@moravia.com>
2026-03-24 14:11:14 +0100

    added sql server + DB

--- FastAndFurious/ProductFuriousStore/ProductFuriousStore.AppHost/AppHost.cs
+++ FastAndFurious/ProductFuriousStore/ProductFuriousStore.AppHost/AppHost.cs
@@ -1,24 +1,30 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("cache");
+//var cache = builder.AddRedis("cache");
 
 var sqlPassword = builder.AddParameter("sql-password", "CsharpAcademy@2026");
 
-var sqlServer = builder.AddSqlServer("fastfurioussqlserver")
-    .WithEndpoint("localhost", 1433)
-    .WithUser("sa")
-    .WithPassword(sqlPassword)
-    .WithDatabase("ProductStore")
-    .WaitForMssql();
+var sqlServer = builder.AddSqlServer("fastfurioussqlserver", sqlPassword)
+    .WithImageTag("2025-latest")
+    .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithContainerName("productStoreSql")
+    //.WithEndpoint(name:"ssms", port: 1433, targetPort: 1433 )
+    ;
+
+var sqlDb = sqlServer.AddDatabase("productstoredb");
+ 
 
 var apiService = builder.AddProject<Projects.ProductFuriousStore_ApiService>("apiservice")
-    .WithHttpHealthCheck("/health");
+    .WithHttpHealthCheck("/health")
+    .WithReference(sqlDb)
+    .WaitForCompletion(sqlDb);
 
 builder.AddProject<Projects.ProductFuriousStore_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithHttpHealthCheck("/health")
-    .WithReference(cache)
-    .WaitFor(cache)
+    //.WithReference(cache)
+    //.WaitFor(cache)
     .WithReference(apiService)
     .WaitFor(apiService);
 
