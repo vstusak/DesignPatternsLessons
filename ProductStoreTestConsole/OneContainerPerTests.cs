0fb8c10ce7c47b7152646a9d5392a63f47afdfc1
Milan Karasek <milan.karasek@rws.com>
2026-07-02 15:23:52 +0200

    Implemented SharedContainerForAllTests

--- ProductStoreTestConsole/OneContainerPerTests.cs
+++ ProductStoreTestConsole/OneContainerPerTests.cs
@@ -79,6 +79,7 @@ namespace ProductStoreTestConsole
         }
     }
 
+    [TestClass]
     public class SharedContainerForAllTests
     {
         private static MsSqlContainer _sqlContainer;
@@ -86,27 +87,27 @@ namespace ProductStoreTestConsole
         private PeopleRepository _peopleRepository;
 
         [ClassInitialize]
-        public static void ClassInitialize(TestContext context)
+        public static async Task ClassInitialize(TestContext context)
         {
-            //TODO - resolve the shared container setup and configuration
             _sqlContainer = new MsSqlBuilder("mcr.microsoft.com/mssql/server:2022-CU14-ubuntu-22.04")
             .Build();
+
+            await _sqlContainer.StartAsync(context.CancellationToken);
         }
 
         [TestInitialize]
         public async Task InitializeAsync()
         {
-            await _sqlContainer.StartAsync();
-
             Console.WriteLine($"SQL Container started! Connection string: {_sqlContainer.GetConnectionString()}");
 
-            var connectionString = _sqlContainer.GetConnectionString();
+            var connectionString = _sqlContainer.GetConnectionString().Replace("Database=master", "Database=PersonDB");
             var options = new DbContextOptionsBuilder<PersonDBContext>()
                 .UseSqlServer(connectionString)
                 .Options;
 
             _dbContext = new PersonDBContext(options);
 
+            await _dbContext.Database.EnsureDeletedAsync();
             await _dbContext.Database.EnsureCreatedAsync();
 
             _peopleRepository = new PeopleRepository(_dbContext);
@@ -115,8 +116,13 @@ namespace ProductStoreTestConsole
         [TestCleanup]
         public async Task CleanupAsync()
         {
-            await _sqlContainer.DisposeAsync();
             await _dbContext.DisposeAsync();
+        }
+
+        [ClassCleanup]
+        public static async Task ClassCleanupAsync()
+        {
+            await _sqlContainer.DisposeAsync();
         }
 
 
