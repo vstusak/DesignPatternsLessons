aef1658195afb9a29415ebeaa8151025ffc0bf78
Pavel Dolezal <PDolezal@moravia.com>
2026-07-16 10:00:01 +0200

    enhancement of the ProductRepo

---
+++ FastAndFurious/ProductFuriousStore/ProductFuriousStore.ApiService/Data/IProductRepo.cs
@@ -0,0 +1,13 @@
+﻿using ProductFuriousStore.Contracts.Entities;
+
+namespace ProductFuriousStore.ApiService.Data
+{
+    public interface IProductRepo
+    {
+        public Product Get(int id);
+        public Product Add(Product product); 
+        public void Delete(int id);
+        public IEnumerable<Product> GetAll();
+        public void Update(Product product);
+    }
+}
--- FastAndFurious/ProductFuriousStore/ProductFuriousStore.ApiService/Endpoints/ProductsGetEndpoint.cs
+++ FastAndFurious/ProductFuriousStore/ProductFuriousStore.ApiService/Endpoints/ProductsGetEndpoint.cs
@@ -1,19 +1,23 @@
+using ProductFuriousStore.ApiService.Data;
+
 namespace ProductFuriousStore.ApiService;
 
 public class ProductsGetEndpoint : IEndpoint
 {
     private readonly ILogger<ProductsGetEndpoint> _logger;
+    private readonly IProductRepo _productRepo;
 
-    public ProductsGetEndpoint(ILogger<ProductsGetEndpoint> logger)
+    public ProductsGetEndpoint(ILogger<ProductsGetEndpoint> logger, IProductRepo productRepo)
     {
         _logger = logger;
+        _productRepo = productRepo;
     }
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
         app.MapGet("/products", () =>
             {
                 _logger.LogDebug("Fetching all products");
-                return Products.Collection;
+                return _productRepo.GetAll();
             })
             .WithName("GetProducts");
     }
